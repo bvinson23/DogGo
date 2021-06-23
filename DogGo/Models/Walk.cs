f0acd91d359e4452853d8da24c1f9577814276b2
bvinson23 <brandonvinson23@gmail.com>
2021-06-23 11:49:55 -0500

    created Walk model to be used in walker details

---
+++ DogGo/Models/Walk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Models
+{
+    public class Walk
+    {
+        public DateTime Date { get; set; }
+        public Owner Client { get; set; }
+        public int Duration { get; set; }
+    }
+}
--- DogGo/Repositories/WalkerRepository.cs
+++ DogGo/Repositories/WalkerRepository.cs
@@ -52,7 +52,6 @@ namespace DogGo.Repositories
                                 Id = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
                                 Name = reader.GetString(reader.GetOrdinal("NeighborhoodName"))
                             }
-
                         };
 
                         walkers.Add(walker);
@@ -73,9 +72,9 @@ namespace DogGo.Repositories
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                        SELECT Id, [Name], ImageUrl, NeighborhoodId
-                        FROM Walker
-                        WHERE Id = @id
+                        SELECT w.Id, w.[Name], w.ImageUrl, w.NeighborhoodId, n.Name as NeighborhoodName
+                        FROM Walker w
+                        JOIN Neighborhood n on n.id = w.NeighborhoodId
                     ";
 
                     cmd.Parameters.AddWithValue("@id", id);
@@ -89,7 +88,12 @@ namespace DogGo.Repositories
                             Id = reader.GetInt32(reader.GetOrdinal("Id")),
                             Name = reader.GetString(reader.GetOrdinal("Name")),
                             ImageUrl = reader.GetString(reader.GetOrdinal("ImageUrl")),
-                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                            Neighborhood = new Neighborhood
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                Name = reader.GetString(reader.GetOrdinal("NeighborhoodName"))
+                            }
                         };
 
                         reader.Close();
